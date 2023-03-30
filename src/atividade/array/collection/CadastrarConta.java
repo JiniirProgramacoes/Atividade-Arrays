1b828e817053b3858f64847b280652663ac7286d
JiniirProgramacoes <juniior.canal.you@gmail.com>
2023-03-30 13:02:10 -0300

    Primeira versão

---
+++ src/atividade/array/collection/CadastrarConta.java
@@ -0,0 +1,57 @@
+package atividade.array.collection;
+
+import atividade.array.object.Conta;
+
+public class CadastrarConta {
+    
+    private Conta[] conta = new Conta[100];
+    private int cont;
+    private int contV;
+
+    
+    public void adicionarConta(Conta conta){
+         this.conta[cont] = conta;
+         cont++;
+    }
+    public void adicionarContaPosicao(Conta conta, int posicao){
+    	if(!verificarPosicao(posicao)) {
+    		throw new IllegalArgumentException("Posição Inválida");
+    	}
+    	contV = cont;
+		while(contV > (posicao - 1)) {
+			this.conta[contV] = this.conta[contV - 1];
+			contV--;
+		}
+		this.conta[contV] = conta;
+		cont++;
+    }
+    
+    public Conta[] listarConta(){
+        return this.conta;
+    }
+    
+    public Conta procurarConta(int i){
+    	if(!verificarPosicao(i)) {
+        	throw new IllegalArgumentException("Posição Inválida");
+    	}
+    	return conta[i];
+    }
+    
+    public Conta procurarContaNome(String nome){
+        for(Conta c : this.conta){
+            if(c.getNome().toLowerCase().equals(nome.toLowerCase())){
+                return c;
+            }
+        }
+        return null;
+    }
+    
+    public boolean verificarPosicao(int posicao) {
+    	if(posicao >= 1 && posicao <= conta.length) {
+    		return true;
+    	}else {
+    		return false;
+    	}
+    }
+    
+}
